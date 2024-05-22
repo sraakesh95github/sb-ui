1f556c0beea897881014e620a2209f60745582d1
sraakesh95github <raakesh95jobs@gmail.com>
2024-05-22 11:57:02 -0400

    Updating variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,20 +3,21 @@ variable "region" {
 }
 
 variable "vpc_cidr" {
-
+  default = "10.0.0.0/16"
 }
 
 variable "bucket" {
-
+  default = "sr-technical"
 }
 
 variable "public_key" {
-
+  default = ""
 }
 
 variable "private_key" {
+  default = ""
 }
 
 variable "key_name" {
-    
+    default = ""
 }
